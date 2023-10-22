1525c4a22bb520ea1c7dfb48750b5500931912af
David Chu <chu19972009@gmail.com>
2023-10-22 21:36:51 +0800

    - modify neural network structure, accuracy -> 96%

--- q2.py
+++ q2.py
@@ -3,7 +3,6 @@ import sklearn
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -29,20 +28,42 @@ def normalize_data(original_data):
     return original_data
 
 
-class Multiclass(nn.Module):
+# nerual network [18 input] [36 hidden] [5 output]
+class Neural_Network_Classification(nn.Module):
     def __init__(self):
         super().__init__()
-        self.hidden = nn.Linear(18, 36)
-        self.act = nn.ReLU()
-        self.output = nn.Linear(36, 5)
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(18, 36),
+            nn.ReLU(),
+            nn.Linear(36, 36),
+            nn.ReLU(),
+            nn.Linear(36, 54),
+            nn.ReLU(),
+            nn.Linear(54, 54),
+            nn.ReLU(),
+            nn.Linear(54, 18),
+            nn.ReLU(),
+            nn.Linear(18, 5),
+        )
 
     def forward(self, x):
-        x = self.act(self.hidden(x))
-        x = self.output(x)
+        x = self.flatten(x)
+        x = self.linear_relu_stack(x)
         return x
 
 
 if __name__ == "__main__":
+    # check if GPU is available
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    print(f"Using device: {device}")
+
     # reading the training data
     training_data = pd.read_csv("data-release/data2/training.csv")
     # cleaning empty cell inside the training_data
@@ -50,38 +71,51 @@ if __name__ == "__main__":
     # normalizing 18 columns excluding the id and the class label
     normalized_training_data = normalize_data(cleaned_training_data)
 
-    # split data into X and y
-    X = cleaned_training_data.iloc[:, 1:19]
-    Y = cleaned_training_data.iloc[:, 19:]
-    ohe = OneHotEncoder(handle_unknown="ignore", sparse_output=False).fit(Y)
-    Y = ohe.transform(Y)
+    # reading the training data
+    validation_data = pd.read_csv("data-release/data2/validation.csv")
+    # cleaning empty cell inside the training_data
+    cleaned_validation_data = cleaning_dirty_data(training_data)
+    # normalizing 18 columns excluding the id and the class label
+    normalized_validation_data = normalize_data(cleaned_validation_data)
 
-    # nerual network [18 input] [36 hidden] [5 output]
-    model = Multiclass()
+    # split training data into X and y
+    X_train = cleaned_training_data.iloc[:, 1:19]
+    Y_train = cleaned_training_data.iloc[:, 19:]
+    ohe = OneHotEncoder(handle_unknown="ignore", sparse_output=False).fit(Y_train)
+    Y_train = ohe.transform(Y_train)
+
+    # split validation data into X and y
+    X_validate = cleaned_validation_data.iloc[:, 1:19]
+    Y_validate = cleaned_validation_data.iloc[:, 19:]
+    ohe = OneHotEncoder(handle_unknown="ignore", sparse_output=False).fit(Y_validate)
+    Y_validate = ohe.transform(Y_validate)
+
+    # nerual network
+    model = Neural_Network_Classification()
     loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     # convert pandas DataFrame (X) and numpy array (y) into PyTorch tensors
-    X = X.astype(float)
-    X = torch.tensor(X.values, dtype=torch.float)
-    Y = torch.tensor(Y, dtype=torch.float)
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, Y, train_size=0.7, shuffle=True
-    )
+    X_train = X_train.astype(float)
+    X_train = torch.tensor(X_train.values, dtype=torch.float)
+    Y_train = torch.tensor(Y_train, dtype=torch.float)
+    X_validate = X_validate.astype(float)
+    X_validate = torch.tensor(X_validate.values, dtype=torch.float)
+    Y_validate = torch.tensor(Y_validate, dtype=torch.float)
 
     # training parameters
-    n_epochs = 50
-    batch_size = 5
-    batches_per_epoch = len(X) // batch_size
+    n_epochs = 200
+    batch_size = 20
+    batches_per_epoch = len(X_train) // batch_size
 
-    best_acc = -np.inf  # init to negative infinity
+    best_acc = -np.inf
     best_weights = None
     train_loss_hist = []
     train_acc_hist = []
     test_loss_hist = []
     test_acc_hist = []
 
+    # training loop
     for epoch in range(n_epochs):
         epoch_loss = []
         epoch_acc = []
@@ -93,7 +127,7 @@ if __name__ == "__main__":
                 # take a batch
                 start = i * batch_size
                 X_batch = X_train[start : start + batch_size]
-                y_batch = y_train[start : start + batch_size]
+                y_batch = Y_train[start : start + batch_size]
                 # forward pass
                 y_pred = model(X_batch)
                 loss = loss_fn(y_pred, y_batch)
@@ -112,9 +146,9 @@ if __name__ == "__main__":
 
         # set model in evaluation mode and run through the test set
         model.eval()
-        y_pred = model(X_test)
-        ce = loss_fn(y_pred, y_test)
-        acc = (torch.argmax(y_pred, 1) == torch.argmax(y_test, 1)).float().mean()
+        y_pred = model(X_validate)
+        ce = loss_fn(y_pred, Y_validate)
+        acc = (torch.argmax(y_pred, 1) == torch.argmax(Y_validate, 1)).float().mean()
         ce = float(ce)
         acc = float(acc)
         train_loss_hist.append(np.mean(epoch_loss))
